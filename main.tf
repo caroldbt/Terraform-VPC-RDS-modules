5a2252735338f6eb80f9decfe103ed9747305e0e
Carol Burgos <37454302+caroldbt@users.noreply.github.com>
2024-06-25 20:06:51 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+module "vpc"{
+    source ="./modules/vpc"
+    aws_region = "us-east-1"
+    vpc_cidr_block = "172.16.0.0/16"
+    subnet_cidr_block = "172.16.10.0/24"
+    
+}
+
+module "compute"{
+    source ="./modules/compute"
+    subnet = module.vpc.my_subnet
+    myvpc = module.vpc.my_vpc
+    instance_type = "t2.micro"
+    key_name = "vockey"
+    ami = "ami-08a0d1e16fc3f61ea"
+}
